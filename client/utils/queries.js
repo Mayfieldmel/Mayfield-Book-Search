3d3664671ad505ebe52afc26c3cbee7f48bb8036
Melissa Mayfield <Mayfieldmel@gmail.com>
2022-10-23 18:26:51 -0400

    update user and book frontend queries

---
+++ client/utils/queries.js
@@ -0,0 +1,63 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      bookCount
+      books {
+        authors: [String], 
+        description: String, 
+        title: String, 
+        bookId: String, 
+        image: String, 
+        link: String
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+        _id
+      username
+      email
+      bookCount
+      books {
+      authors: [String], 
+      description: String, 
+      title: String, 
+      bookId: String, 
+      image: String, 
+      link: String
+    }
+  }
+`;
+
+export const QUERY_BOOKS = gql `
+{
+ books {
+    authors: [String], 
+    description: String, 
+    title: String, 
+    bookId: String, 
+    image: String, 
+    link: String
+  }
+}
+`;
+
+export const  QUERY_SAVEDBOOKS = gql `
+    query savedBooks($username: String) {
+        savedBooks(username: $username) {
+            authors: [String], 
+            description: String, 
+            title: String, 
+            bookId: String, 
+            image: String, 
+            link: String
+        }
+    }
+`
