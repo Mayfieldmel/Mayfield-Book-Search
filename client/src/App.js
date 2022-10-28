246268a1e344fb1b5c89efdf72fcfc1d9bf49d07
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-28 13:42:15 -0400

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,49 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SearchBooks from "./pages/SearchBooks";
-import SavedBooks from "./pages/SavedBooks";
-import Navbar from "./components/Navbar";
-// import ApolloProvider destructured  from apollo
-import { ApolloProvider } from '@apollo/client';
-// import the apollo client from apollo boost
-import ApolloClient from "apollo-boost";
+import React from 'react';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
-// assign in client to a new apollo client in high order function that takes request as a first are than a function then an operation set context with the trinary to check for the graphql uri
-const client = new ApolloClient({
-  request: operation => {
-    const token = localStorage.getItem("id_token");
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
+// first, establish a new link to the GraphQL server at its /graphql endpoint
+const httpLink = createHttpLink({
   uri: "/graphql",
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+// second, instantiate the Apollo Client instance and create the connection to the API endpoint.
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  // instantiate a new cache object
+  cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={SearchBooks} />
-            <Route exact path="/saved" component={SavedBooks} />
-            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
     </ApolloProvider>
   );
 }
